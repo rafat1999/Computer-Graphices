bcb05c69a85f2885d9925822be698cac80283dd8
Shihab Hossen Rafat <100786024+rafat1999@users.noreply.github.com>
2023-03-29 21:38:26 +0600

    Add files via upload

---
+++ Circle Drawing/Circle_drawing.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<graphics.h>
+#include<math.h>
+#include<cmath>
+
+using namespace std;
+int main()
+{
+
+    int gd=DETECT, gm;
+    initgraph(&gd, &gm,"");
+
+    const double pi = 3.1416;
+
+    int r, xc, yc, angle;
+
+    float x, y;
+    cout<< "Enter the center point(xc): ";
+    cin>> xc;
+    cout<< "Enter the center point(yc): ";
+    cin>> yc;
+    cout<< "Enter the redius of the circle: ";
+    cin>> r;
+
+    cout<< "Enter the angle: ";
+    cin>> angle;
+
+    for(int i = 0; i<= angle; i++)
+    {
+        float radians = i *( pi / 180);
+        float c = cos(radians);
+        float s = sin(radians);
+        x = xc + (r * c);
+        y = yc + (r * s);
+        putpixel((int)(x), (int)(y), WHITE);
+    }
+
+    getch();
+    closegraph();
+}
---
+++ Circle Drawing/mid_point_circle.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<graphics.h>
+#include<math.h>
+#include<cmath>
+
+using namespace std;
+int main()
+{
+    int gd=DETECT, gm;
+    initgraph(&gd, &gm,"");
+
+    int r, xc, yc;
+
+    cout<< "Enter the center point(xc): ";
+    cin>> xc;
+    cout<< "Enter the center point(yc): ";
+    cin>> yc;
+    cout<< "Enter the redius of the circle: ";
+    cin>> r;
+    int x = 0;
+    int y = r;
+    putpixel(x,y, WHITE);
+
+    int pk = 1- r;
+    while(x < y)
+    {
+        if(pk < 0)
+        {
+            x++;
+            pk = pk + (2 * x) +1;
+            putpixel(x+xc, y+yc, WHITE);
+            putpixel(-x+xc, y+yc, WHITE);
+            putpixel(x+xc, -y+yc, WHITE);
+            putpixel(-x+xc, -y+yc, WHITE);
+            putpixel(y+xc, x+yc, WHITE);
+            putpixel(-y+xc, x+yc, WHITE);
+            putpixel(y+xc, -x+yc, WHITE);
+            putpixel(-y+xc, -x+yc, WHITE);
+
+        }
+        else
+        {
+            x++;
+            y--;
+            pk = pk + (2 * x) + 1 - (2 * y);
+            putpixel(x+xc, y+yc, WHITE);
+            putpixel(-x+xc, y+yc, WHITE);
+            putpixel(x+xc, -y+yc, WHITE);
+            putpixel(-x+xc, -y+yc, WHITE);
+            putpixel(y+xc, x+yc, WHITE);
+            putpixel(-y+xc, x+yc, WHITE);
+            putpixel(y+xc, -x+yc, WHITE);
+            putpixel(-y+xc, -x+yc, WHITE);
+        }
+
+    }
+    getch();
+    closegraph();
+}
